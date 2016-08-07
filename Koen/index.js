844ab81ca6e9dd01ad0e16f3d46118d012fd9560
Maarten van Beek <maartenvanbeek@gmail.com>
2016-08-07 18:39:00 +0200

    Update

---
+++ Koen/index.js
@@ -0,0 +1,21 @@
+var crossindex = 0;
+var crosscount = 4;
+var crossspeed = 5000;
+
+var crossfade = function(){
+	var newindex = (crossindex + 1)%4;
+
+	var current = $("#crossfader")[0].children[crossindex];
+	var next = $("#crossfader")[0].children[newindex];
+
+	current.className = "";
+	next.className = "active";
+
+	crossindex = newindex;
+
+	setTimeout(crossfade, crossspeed);
+}
+
+$(document).ready(function(){
+	setTimeout(crossfade, crossspeed);
+});
